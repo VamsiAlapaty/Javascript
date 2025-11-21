1eb8d306071b33d32ad085f9b0a80bad8d0976a0
VamsiAlapaty <saivamsi7996@gmail.com>
2025-11-21 12:28:24 -0800

    findvalue_logic

---
+++ findValue.js
@@ -0,0 +1,9 @@
+//Finding the elements in the Array defined
+let a = 5;
+let b = 20;
+let arr=[13,76,43,32,10,98,76,7,4,20,78];
+for(let i of arr){
+    if(i == a){
+        console.log("A Element found " + a);
+    }
+}
