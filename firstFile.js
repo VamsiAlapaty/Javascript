23caf47e69cffae370d5172ade960e071180edff
VamsiAlapaty <saivamsi7996@gmail.com>
2025-11-23 11:15:34 -0800

    CountOfBiggerCandles

--- firstFile.js
+++ firstFile.js
@@ -1,12 +1,10 @@
 //Find the lowest value in the array
 let arr=[78,98,112,34,39,23,56,4,17,48];
 let len = arr.length;
-let j=1;
 let value = arr[0];
 for(i of arr){
     if(value > i){
         value=i;
     }
-    j++;
 }
 console.log(value);
--- secondFile.js
+++ secondFile.js
@@ -1,1 +1,16 @@
 console.log("This is second File");
+//Count of candles that are big among other
+let arr=[3,4,5,2,7,5,4,7,4,7];
+let len = arr.length;
+let count=0;
+let high = arr[0];
+for(let i of arr){
+    if(high<i){
+        high = i;
+    }else if(high == i){
+        console.log(high + " " + i);
+        count++;
+    } 
+}
+console.log(high);
+console.log(count);
